99435b4af6ead5752a4c1eabb08791cc0f5bdc81
naTalieH3 <nnhenry@millersville.edu>
2025-03-18 10:37:47 -0400

    Add files via upload

---
+++ Lab2/dist/hello.7e048d94.js
@@ -0,0 +1,22 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+var hour;
+if (hourNow > 18) {
+    greeting = 'Good evening';
+    hour = hourNow - 12;
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+    hour = hourNow - 12;
+} else if (hourNow > 0) {
+    greeting = 'Good morning';
+    hour = hourNow;
+} else greeting = 'Welcome';
+document.write(greeting);
+document.write("<br>");
+document.write("The time is ");
+document.write(hour);
+document.write(":");
+document.write(today.getMinutes());
+
+//# sourceMappingURL=hello.7e048d94.js.map
